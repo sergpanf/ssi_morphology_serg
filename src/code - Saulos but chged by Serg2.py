9cfef4f594b4c9d70093f52ed2426d7a02649456
Sergey Panfilov <sergpanf@gmail.com>
2025-09-15 13:38:16 -0400

    AI test norm+lemma+case+gender+person+number+mood+sp+tense features

--- src/code - Saulos but chged by Serg2.py
+++ src/code - Saulos but chged by Serg2.py
@@ -110,11 +110,11 @@ main(["-mo", "train", "-i", "input_NT_normalized", "-o", "output_NT_normalized_X
 
 # %% TRAINING with EVAL flag
 
-main(["-mo", "train", "-i", "input_NT_nlcgpS", "-o", "output_NT_XYs", "-ep", "20", "-l", "10", "-lr", "0.0001", "-et", "True"]) # using older output file, as XY-phrase-pattern will stay the same and the word count of the input file stays equal with it
+main(["-mo", "train", "-i", "input_NT_nlcgpnmst", "-o", "output_NT_XYs", "-ep", "20", "-l", "10", "-lr", "0.0001", "-et", "True"]) # using older output file, as XY-phrase-pattern will stay the same and the word count of the input file stays equal with it
 
 
 # %% F-SCORE of the MODEL
-file = "../sp_evaluation_results_transformer/input_NT_nlcgpS_output_NT_XYs_ONE_DATASET/results_10seq_len_0.0001lr_512embsize_8nhead_transformer_0.1dropout_128_batchsize_20epochs_3beamsize.txt"
+file = "../sp_evaluation_results_transformer/input_NT_nlcgpnmst_output_NT_XYs_ONE_DATASET/results_10seq_len_0.0001lr_512embsize_8nhead_transformer_0.1dropout_128_batchsize_20epochs_3beamsize.txt"
 
 TP = 0
 FP = 0
